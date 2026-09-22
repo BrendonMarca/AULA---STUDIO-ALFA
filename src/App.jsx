34931b7cde90df652b7bd0833f3500383493d43f
Professor SENAI <Professor@SN-358603.edu.fiesc.com.br>
2026-09-22 17:19:51 -0300

    Commit inicial do projeto

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import Header from "./components/Header/Header"
+
+
+function App(){
+  return(
+    <>
+      <Header/>
+      {/* <Main></Main>
+      <Footer></Footer> */}
+    </>
+  )
+}
+
+export default App
