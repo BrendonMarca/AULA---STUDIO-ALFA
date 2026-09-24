dc38592b5327d0667117f5933107463a45daae14
Professor SENAI <Professor@SN-358603.edu.fiesc.com.br>
2026-09-24 16:04:47 -0300

    Segundo commit

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,19 +1,23 @@
+import './Header.css'
 
 function Header() {
-    return(
-        <header className="header">
-            <div className="logo">
-                <img className="logo-icon" src="#" alt="Logo" />
-                <span className="logo-text">Studio Alfa</span>
-            </div>
-            <nav className="nav">
-                <a href="#">Início</a>
-                <a href="#">Serviços</a>
-                <a href="#">Sobre</a>
-                <a href="#" className="btn-contatos">Contato</a>
-            </nav>
-        </header>
-    )
+  return (
+    <header className="header">
+      <div className="header-container">
+        <div className="logo">
+          <img className='logo-icon' src="../../../src/assets/imgs/alfa_romeo_black-logo_brandlogos.net_f3e8a.png" alt="" />
+          <span className="logo-text">Studio Alfa</span>
+        </div>
+
+        <nav className="nav">
+          <a href="#inicio">Inicio</a>
+          <a href="#servicos">Servicos</a>
+          <a href="#sobre">Sobre</a>
+          <a href="#contato" className="btn-contato">Contato</a>
+        </nav>
+      </div>
+    </header>
+  )
 }
 
 export default Header
